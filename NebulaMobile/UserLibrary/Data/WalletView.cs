09071fa64ad20b84b85cc7118a1fd19e8615a9dc
Wizard Young <rcvbuf@gmail.com>
2021-11-27 08:13:08 +0800

    run on android

---
+++ NebulaMobile/UserLibrary/Data/WalletView.cs
@@ -0,0 +1,13 @@
+﻿using DexServer.Ext;
+
+namespace UserLibrary.Data
+{
+    public class WalletView : ExtAssert
+    {
+        public decimal MyBalance { get; set; }
+        public decimal DexBalance { get; set; }
+
+        public string Address { get; set; }
+        public string DexWalletID { get; set; }
+    }
+}
